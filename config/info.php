967aa9998107fa362c70c5eafd019469af2e2218
Noviyanto Rahmadi <novay@enterwind.com>
2020-07-03 19:38:47 +0800

    Installer

---
+++ config/info.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'software' => [
+        'name'      => 'Feedy',
+        'author'    => 'Noviyanto Rahmadi',
+        'url'       => 'https://feedy.btekno.id',
+        'version'   => '1.0.0'
+    ]
+
+];
---
+++ config/install.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Validation\Rule;
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Server Requirements
+    |--------------------------------------------------------------------------
+    */
+    'php_version' => '7.2.5',
+
+    'extensions' => [
+        'php' => [
+            'BCMath',
+            'Ctype',
+            'Fileinfo',
+            'JSON',
+            'Mbstring',
+            'OpenSSL',
+            'PDO',
+            'Tokenizer',
+            'XML',
+            'GD',
+            'cURL'
+        ],
+        'apache' => [
+            'mod_rewrite',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | File permissions
+    |--------------------------------------------------------------------------
+    */
+    'permissions' => [
+        'Files' => [
+            '.env',
+        ],
+        'Folders' =>
+        [
+            'bootstrap/cache',
+            'resources/lang',
+            'storage',
+            'storage/framework/',
+            'storage/framework/cache',
+            'storage/framework/sessions',
+            'storage/framework/views',
+            'storage/logs',
+        ],
+    ]
+];
